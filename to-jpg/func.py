c6fd9831f0281bc817552f6b3313a83bc3c6f75e
Antoine Sion <antoine.sion@mines-paristech.fr>
2022-02-07 18:10:31 +0100

    fix empty latex document

--- to-jpg/func.py
+++ to-jpg/func.py
@@ -68,9 +68,9 @@ def handler(ctx, data: io.BytesIO = None):
 
         document_latex = re.search(rb"\\begin{document}\s*(?:\\fontsize{.*?}{.*?}\\selectfont)?\s*([\s\S]*?)\s*\\end{document}",
                                    latex, flags=re.MULTILINE)
-        if document_latex and document_latex.group(1).strip() == b"":
+        if document_latex and document_latex.group(1) == b"":
             latex = latex[:document_latex.start(
-                1)] + b"\\phantom{ }" + latex[document_latex.end(1):]
+                1)] + b"\\phantom{?}" + latex[document_latex.end(1):]
 
         input_file, input_file_path = mkstemp(dir=COMPILATION_DIR)
         input_filename = os.path.split(input_file_path)[1]
