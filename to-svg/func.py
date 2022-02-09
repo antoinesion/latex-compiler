fddadb0aa5740a37105370a9111443f05dd46ae0
Antoine Sion <antoine.sion@mines-paristech.fr>
2022-02-09 09:50:05 +0100

    configure sentry

--- to-svg/func.py
+++ to-svg/func.py
@@ -33,132 +33,134 @@ LATEX_TEMPLATE = b"""\\batchmode
 
 
 def handler(ctx, data: io.BytesIO = None):
-    os.chdir(COMPILATION_DIR)
+    with sentry_sdk.start_transaction(op="task", name="to-svg"):
+        os.chdir(COMPILATION_DIR)
 
-    width = 595  # A4 width
-    padding = 3
-    font_size = 10
-    baseline_skip = 1.2
-    packages = ''
-    latex = None
+        width = 595  # A4 width
+        padding = 3
+        font_size = 10
+        baseline_skip = 1.2
+        packages = ''
+        latex = None
 
-    try:
         try:
-            decoder = MultipartDecoder(
-                data.read(), ctx.Headers()["content-type"])
-            for field in decoder.parts:
-                field_name = field.headers[b"Content-Disposition"].decode().split(";")[
-                    1].split("=")[1][1:-1]
-                if field_name == "width":
-                    width = float(field.content)
-                if field_name == "padding":
-                    padding = float(field.content)
-                if field_name == "font_size":
-                    font_size = float(field.content)
-                if field_name == "baseline_skip":
-                    baseline_skip = float(field.content)
-                if field_name == "packages":
-                    packages = field.content
-                if field_name == "latex":
-                    latex = field.content
-                if field_name == "image[]":
-                    filename = field.headers[b"Content-Disposition"].decode().split(";")[
-                        2].split("=")[1][1:-1]
-                    with open(filename, "wb") as f:
-                        f.write(field.content)
-        except Exception as e:
-            encoder = MultipartEncoder({
-                "message": "cannot parse form data",
-                "code": "parsing_error",
-                "error": str(e)
+            try:
+                decoder = MultipartDecoder(
+                    data.read(), ctx.Headers()["content-type"])
+                for field in decoder.parts:
+                    field_name = field.headers[b"Content-Disposition"].decode().split(";")[
+                        1].split("=")[1][1:-1]
+                    if field_name == "width":
+                        width = float(field.content)
+                    if field_name == "padding":
+                        padding = float(field.content)
+                    if field_name == "font_size":
+                        font_size = float(field.content)
+                    if field_name == "baseline_skip":
+                        baseline_skip = float(field.content)
+                    if field_name == "packages":
+                        packages = field.content
+                    if field_name == "latex":
+                        latex = field.content
+                    if field_name == "image[]":
+                        filename = field.headers[b"Content-Disposition"].decode().split(";")[
+                            2].split("=")[1][1:-1]
+                        with open(filename, "wb") as f:
+                            f.write(field.content)
+            except Exception as e:
+                encoder = MultipartEncoder({
+                    "message": "cannot parse form data",
+                    "code": "parsing_error",
+                    "error": str(e)
+                })
+                return response.Response(
+                    ctx, response_data=encoder.to_string(),
+                    headers={"Content-Type": encoder.content_type},
+                    status_code=BAD_REQUEST)
+
+            if latex == None:
+                encoder = MultipartEncoder({
+                    "message": "'latex' field is missing in form data",
+                    "code": "latex_missing"
+                })
+                return response.Response(
+                    ctx, response_data=encoder.to_string(),
+                    headers={"Content-Type": encoder.content_type},
+                    status_code=BAD_REQUEST)
+
+            input_file, input_file_path = mkstemp(dir=COMPILATION_DIR)
+            input_filename = os.path.split(input_file_path)[1]
+
+            os.write(input_file, LATEX_TEMPLATE % {
+                b'width': width,
+                b'padding': padding,
+                b'font_size': font_size,
+                b'baseline_skip': font_size * baseline_skip,
+                b'packages': packages,
+                b'latex': latex
             })
-            return response.Response(
-                ctx, response_data=encoder.to_string(),
-                headers={"Content-Type": encoder.content_type},
-                status_code=BAD_REQUEST)
+            os.close(input_file)
 
-        if latex == None:
-            encoder = MultipartEncoder({
-                "message": "'latex' field is missing in form data",
-                "code": "latex_missing"
-            })
-            return response.Response(
-                ctx, response_data=encoder.to_string(),
-                headers={"Content-Type": encoder.content_type},
-                status_code=BAD_REQUEST)
+            call(['pdflatex', '-shell-escape', input_filename])
+            output_filename = input_filename + '.svg'
+            try:
+                with open(output_filename, "r") as f:
+                    pass
+            except:
+                for tmp_file in glob.glob(input_filename + '*'):
+                    os.remove(tmp_file)
 
-        input_file, input_file_path = mkstemp(dir=COMPILATION_DIR)
-        input_filename = os.path.split(input_file_path)[1]
+                encoder = MultipartEncoder({
+                    "message": "compilation failed",
+                    "code": "latex_error"
+                })
+                return response.Response(
+                    ctx, response_data=encoder.to_string(),
+                    headers={"Content-Type": encoder.content_type},
+                    status_code=BAD_REQUEST)
 
-        os.write(input_file, LATEX_TEMPLATE % {
-            b'width': width,
-            b'padding': padding,
-            b'font_size': font_size,
-            b'baseline_skip': font_size * baseline_skip,
-            b'packages': packages,
-            b'latex': latex
-        })
-        os.close(input_file)
+            optimized_filename = input_filename + '.min.svg'
+            call(["svgo", "--config", "/function/svgo.config.js",
+                  output_filename, "-o", optimized_filename])
+            with open(optimized_filename, 'r') as f:
+                svg = f.read()
 
-        call(['pdflatex', '-shell-escape', input_filename])
-        output_filename = input_filename + '.svg'
-        try:
-            with open(output_filename, "r") as f:
-                pass
-        except:
+            svg = svg.replace("stroke:#000;", "")
+            svg = svg.replace("fill:#000;", "")
+            width_attr = re.search(r'width="[0-9.]*"\s', svg)
+            svg = svg[:width_attr.start()] + svg[width_attr.end():]
+            height_attr = re.search(r'height="[0-9.]*"\s', svg)
+            svg = svg[:height_attr.start()] + svg[height_attr.end():]
+            view_box_width = re.search(
+                r'viewBox="[0-9.]* [0-9.]* ([0-9.]*) [0-9.]*"', svg)
+            svg_width = float(
+                svg[view_box_width.start(1):view_box_width.end(1)])
+            if svg_width / (width+padding*2) < 0.9:
+                svg = svg[:view_box_width.start(
+                    1)] + str(width+padding*2) + svg[view_box_width.end(1):]
+
             for tmp_file in glob.glob(input_filename + '*'):
                 os.remove(tmp_file)
 
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
             encoder = MultipartEncoder({
-                "message": "compilation failed",
-                "code": "latex_error"
+                "message": "unknown error",
+                "code": "unknown_error",
             })
             return response.Response(
                 ctx, response_data=encoder.to_string(),
                 headers={"Content-Type": encoder.content_type},
-                status_code=BAD_REQUEST)
+                status_code=INTERNAL_SERVER_ERROR
+            )
 
-        optimized_filename = input_filename + '.min.svg'
-        call(["svgo", "--config", "/function/svgo.config.js",
-             output_filename, "-o", optimized_filename])
-        with open(optimized_filename, 'r') as f:
-            svg = f.read()
-
-        svg = svg.replace("stroke:#000;", "")
-        svg = svg.replace("fill:#000;", "")
-        width_attr = re.search(r'width="[0-9.]*"\s', svg)
-        svg = svg[:width_attr.start()] + svg[width_attr.end():]
-        height_attr = re.search(r'height="[0-9.]*"\s', svg)
-        svg = svg[:height_attr.start()] + svg[height_attr.end():]
-        view_box_width = re.search(
-            r'viewBox="[0-9.]* [0-9.]* ([0-9.]*) [0-9.]*"', svg)
-        svg_width = float(svg[view_box_width.start(1):view_box_width.end(1)])
-        if svg_width / (width+padding*2) < 0.9:
-            svg = svg[:view_box_width.start(
-                1)] + str(width+padding*2) + svg[view_box_width.end(1):]
-
-        for tmp_file in glob.glob(input_filename + '*'):
-            os.remove(tmp_file)
-
-    except Exception as e:
         encoder = MultipartEncoder({
-            "message": "unknown error",
-            "code": "unknown_error",
-            "error": str(e),
+            "message": "compilation succeeded",
+            "code": "success",
+            "svg": ("result.svg", svg, "image/svg+xml")
         })
         return response.Response(
             ctx, response_data=encoder.to_string(),
             headers={"Content-Type": encoder.content_type},
-            status_code=INTERNAL_SERVER_ERROR
+            status_code=OK
         )
-
-    encoder = MultipartEncoder({
-        "message": "compilation succeeded",
-        "code": "success",
-        "svg": ("result.svg", svg, "image/svg+xml")
-    })
-    return response.Response(
-        ctx, response_data=encoder.to_string(),
-        headers={"Content-Type": encoder.content_type},
-        status_code=OK
-    )
