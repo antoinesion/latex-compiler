202a840d281413abed517588a4b4404ffb90e1d3
Antoine Sion <antoine.sion@mines-paristech.fr>
2022-03-10 10:29:19 +0100

    try fix svgo issue

--- to-svg/func.py
+++ to-svg/func.py
@@ -140,9 +140,9 @@ def handler(ctx, data: io.BytesIO = None):
             with open(optimized_filename, 'r') as f:
                 svg = f.read()
 
-            svg = svg.replace("fill:#000;", "fill:currentColor;")
-            svg = svg.replace("stroke:#000;", "stroke:currentColor;")
-            svg = svg.replace('fill-opacity:1;', '')
+            svg = svg.replace('fill="#000"', 'fill="currentColor"')
+            svg = svg.replace('stroke="#000"', 'stroke="currentColor"')
+            svg = svg.replace(' fill-opacity="1"', '')
             svg = re.sub(r'width="[0-9.]*(pt)?"\s', '', svg, 1)
             svg = re.sub(r'height="[0-9.]*(pt)?"\s', '', svg, 1)
             view_box_width = re.search(
